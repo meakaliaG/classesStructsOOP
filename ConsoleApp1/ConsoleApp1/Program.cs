6090542f4112b3009db266df1fb0ad310fff6438
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-29 08:13:15 -0400

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace classes_structs_oop
+{
+    public class Character
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
