0d441ee4e58c4f6684ac69f388177ec47aca7f83
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-29 08:45:31 -0400

    Update Program.cs
    up to reset()

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,6 +17,27 @@
             Console.WriteLine("Hero: " + name + " - " + exp + " EXP");
         }
 
+        //private method
+        private void Reset()
+        {
+            this.name = "Not assigned";
+            this.exp = 0;
+        }
+
+    }
+    public struct Weapon
+    {
+        public string name;
+        public int damage;
+        public Weapon(string name, int damage)
+        {
+            this.name = name;
+            this.damage = damage;
+        }
+        public void PrintWeaponStats()
+        {
+            Console.WriteLine("Weapon: " + this.name + " - " + this.damage + " DMG");
+        }
     }
     internal class Program
     {
@@ -29,6 +50,31 @@
 
             Character heroine = new Character("Agatha");
             heroine.PrintStatsInfo();
+
+            //weapon draw
+            Weapon huntingBow = new Weapon("Hunting Bow", 105);
+            huntingBow.PrintWeaponStats();
+
+            //Testing reference types
+            Character villain = hero;
+
+            villain.name = "Cloudy Meatballs";
+
+            hero.PrintStatsInfo();
+            villain.PrintStatsInfo();
+
+            //Testing value types
+            //note: hunting bow is declared above
+            Weapon warBow = huntingBow;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
+
+            warBow.name = "War Bow";
+            warBow.damage = 155;
+
+            huntingBow.PrintWeaponStats();
+            warBow.PrintWeaponStats();
         }
     }
 }
